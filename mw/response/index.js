244eb1082d5681636c87bd9fb2d9901dd3f86182
mikehajj <mike.hajj@gmail.com>
2019-09-19 12:04:46 +0300

    reverted code

--- mw/response/index.js
+++ mw/response/index.js
@@ -33,31 +33,20 @@ module.exports = function (configuration) {
             return response;
         };
         if (configuration.controllerResponse) {
-            req.soajs.controllerResponse = function (jsonObject) {
-                
-	            if(jsonObject.error && jsonObject.status && process.env.SOAJS_ERROR2HTTP){
-		            res.writeHead(jsonObject.status);
-		            res.end(jsonObject.msg);
-	            }
-	            else{
-                    let jsonObj = {};
-                    if (req.soajs.buildResponse && jsonObject.code && jsonObject.msg)
-                        jsonObj = req.soajs.buildResponse(jsonObject);
-    
-                    if(jsonObj && jsonObject.status){
-                        jsonObj.status = jsonObject.status;
-                    }
-                
-                    var headObj = jsonObj.headObj || {};
-                    headObj['Content-Type'] = 'application/json';
-                    if (jsonObj.status)
-                        res.writeHead(jsonObj.status, headObj);
-                    else
-                        res.writeHead(200, headObj);
-    
-                    res.end(JSON.stringify(jsonObj));
-                }
-            };
+          req.soajs.controllerResponse = function (jsonObj) {
+            if (req.soajs.buildResponse && jsonObj.code && jsonObj.msg)
+              jsonObj = req.soajs.buildResponse(jsonObj);
+        
+        
+            var headObj = jsonObj.headObj || {};
+            headObj['Content-Type'] = 'application/json';
+            if (jsonObj.status)
+              res.writeHead(jsonObj.status, headObj);
+            else
+              res.writeHead(200, headObj);
+        
+            res.end(JSON.stringify(jsonObj));
+          };
         }
         next();
     };
